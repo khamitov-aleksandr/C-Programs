d6f0b385409684145a5300fb91c062c269d0b014
khamitov <khamitov.aleksandr@gmail.com>
2023-01-25 18:04:47 +0100

    add: if else

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Yes, it is Alex!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
