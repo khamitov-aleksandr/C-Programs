5076544534a1e37262c795e51ad117eae8f3aa7f
khamitov <khamitov.aleksandr@gmail.com>
2023-02-16 13:51:21 +0300

    Add: examples040

---
+++ Examples/Example040_outputArrayWith8Elements/Program.cs
@@ -0,0 +1,21 @@
+﻿// Write a program that outputs an 8-element array filled with zeros and ones in random order.
+Console.Clear();
+
+int[] getArray()
+{
+     int[] array = new int[8];
+     for (int i = 0; i < array.Length; i++)
+     {
+         array[i] = new Random().Next(0, 2);
+     }
+     return array;
+}
+
+void printArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+
+        Console.Write($"{inArray[i]} ");
+}
+int[] array = getArray();
+printArray(array);
