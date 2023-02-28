2fbcc012826ffe9450826557d96faf2cd008706a
khamitov <khamitov.aleksandr@gmail.com>
2023-02-28 11:46:42 +0300

    add: new examples

---
+++ Examples/Example055_firstNFibonacciNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// Without using recursion, print the first N Fibonacci numbers.
+// The first two Fibonacci numbers are 0 and 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+Console.Clear();
+int[] arrayFib(int N)
+{
+    int[] array = new int[N];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+Console.Write("Enter your Number: ");
+int nFib = int.Parse(Console.ReadLine()!);
+Console.WriteLine(String.Join(", ", arrayFib(nFib)));
---
+++ Examples/Example056_copyArray/Program.cs
@@ -0,0 +1,21 @@
+﻿// Write a program that will create a
+// copy of the given array using element-by-element copying.
+Console.Clear();
+
+int[] CopyArray(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+int[] array = { 1, 2, 3, 55 };
+
+int[] newArray = CopyArray(array);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine();
+Console.WriteLine(String.Join(", ", newArray));
